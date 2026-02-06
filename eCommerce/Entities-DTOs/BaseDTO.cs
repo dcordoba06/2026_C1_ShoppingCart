bac5e2560c771cb8a4472ae9462cb0c02d8e2b36
Dennis Cordoba <dennis.cordoba@gmail.com>
2026-02-05 19:26:13 -0600

    Inclusion del proyecto de entidades.

---
+++ eCommerce/Entities-DTOs/BaseDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_DTOs
+{
+    public class BaseDTO
+    {
+        /*
+         * Clase base, papa de todos los DTOs o POJOS
+         */
+
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public DateTime Created {  get; set; }
+        public DateTime Updated { get; set; }
+
+    }
+}
---
+++ eCommerce/Entities-DTOs/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Entities_DTOs
+{
+    public class Product : BaseDTO
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
